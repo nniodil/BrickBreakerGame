036636f0405ae2ed8f56cab3c5e7c048edde3d4d
Nadir Niodil <165339957+nniodil@users.noreply.github.com>
2024-08-19 18:33:48 +0200

    Update Start Menu.cs

--- Scripts/Start Menu.cs
+++ Scripts/Start Menu.cs
@@ -5,21 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void StartOnMenu()
     {
         SceneManager.LoadScene("SampleScene");
     }
-
-
 }
