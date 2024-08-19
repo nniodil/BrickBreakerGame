d1027facae3add350207378513a381309b4b11f6
Nadir Niodil <165339957+nniodil@users.noreply.github.com>
2024-08-19 18:33:30 +0200

    Update PowerUpScript.cs

--- Scripts/PowerUpScript.cs
+++ Scripts/PowerUpScript.cs
@@ -5,20 +5,12 @@ using UnityEngine;
 public class PowerUpScript : MonoBehaviour
 {
     public float speed;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
     void Update()
     {
         transform.Translate(new Vector2 (0f,-1f) * Time.deltaTime * speed);
 
         if (transform.position.y < -5.6f)
-            
         {
             Destroy(gameObject);
         }
